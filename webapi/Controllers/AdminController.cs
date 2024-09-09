91e1f559a05af1e5d5ba0113273b9b1b50b8b6ef
subha1345 <official.subhajis@gmail.com>
2024-09-10 00:32:35 +0530

    commit-7: done code to retrieve data

---
+++ webapi/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var dbContext = new AdminContext();
+            var products = dbContext.TblProducts.ToList();
+            return Ok(products);
+        }
+    }
+}
---
+++ webapi/Models/AdminContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace webapi.Models;
+
+public partial class AdminContext : DbContext
+{
+    public AdminContext()
+    {
+    }
+
+    public AdminContext(DbContextOptions<AdminContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblProduct> TblProducts { get; set; }
+
+    public virtual DbSet<TblType> TblTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-EQPKCMK5;Initial Catalog=Admin;Persist Security Info=False;User ID=sa;Password=john1989;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblProduct>(entity =>
+        {
+            entity.ToTable("tblProduct");
+
+            entity.Property(e => e.CreatedOn).HasColumnType("datetime");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<TblType>(entity =>
+        {
+            entity.ToTable("tblType");
+
+            entity.Property(e => e.Type)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
